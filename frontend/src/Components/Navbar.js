078a91827f2cb7cecd4e19093f3d14dbe559ef9e
shimelis <shimelisget2@gmail.com>
2024-11-17 05:39:43 -0800

    logo added

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import Logo from "../assets/Group 45.png"
+import {FaSearch } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div>
-        <div className='bg-slate-700 rounded'>
-        <h1 className='tx'>Save and Invest</h1>
+        <div className='bg-slate-700 rounded flex'>
+        <h1 className='text-white'>Save and Invest</h1>
+        <Link to="#">
+                <img src={Logo}  alt="Save and Invest" className='w-36 h-24'/>
+            </Link>
+            <div className='group'>
+            <input type='text'
+            placeholder='search' 
+            className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline none focus:border-1 focus:border-primary"
+            />
+            <FaSearch/>
+
+            </div>
         </div>
-       
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
+          <ul className='flex bg-amber-600 justify-center space-x-2 gap-y-2'>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/training">Training</Link></li>
