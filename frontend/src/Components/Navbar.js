5c7832b8d6e75d9dd652579a6b4291ecc06471cb
shimelis <shimelisget2@gmail.com>
2024-11-17 04:09:37 -0800

    Link added

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
     <div>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="/training">Training</a></li>
-            <li><a href="/saving">Saving</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/credit">Credit</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/investment">Investment</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/training">Training</Link></li>
+            <li><Link to="/saving">Saving</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/credit">Credit</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/investment">Investment</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
     </div>
