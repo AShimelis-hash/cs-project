ac79bde835753a61412beb71e6e9469481d8727f
shimelis <shimelisget2@gmail.com>
2024-11-17 04:04:56 -0800

    React Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Saving from './Components/Saving';
+import Credit from './Components/Credit';
+import Projects from './Components/Projects';
+import Investment from './Components/Investment';
+import Training from './Components/Training';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/saving" element={<Saving />} />
+      <Route path="/credit" element={<Credit/>} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/investment" element={<Investment />} />
+      <Route path="/training" element={<Training />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+     
+    </Routes>
+     
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>
+            <li><a href="/training">Training</a></li>
+            <li><a href="/saving">Saving</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/credit">Credit</a></li>
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/investment">Investment</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+    </div>
+
+  )
+}
+
+export default Navbar
